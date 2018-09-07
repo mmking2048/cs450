706f4d3fbed8fed25e363eb3d8b64663b4e3ec59
Michael Saelee <lee@soi2.org>
2018-09-07 09:13:25 -0500

    Updating vagrantfiles.

--- vagrantprov.sh
+++ vagrantprov.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get install -y gcc-multilib
+apt-get install -y make
 apt-get install -y qemu-system-x86
 apt-get install -y gdb
 apt-get install -y tmux
+apt-get install -y binutils
 
 echo "set auto-load safe-path /" > /home/vagrant/.gdbinit
+echo "cd /vagrant" >> /home/vagrant/.bashrc
