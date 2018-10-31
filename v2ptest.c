25c9304b45ec0d608ba2cde69d2051cbcb86c665
Michelle King <mmking2048@gmail.com>
2018-10-31 09:04:36 -0500

    typo in prior commit

--- v2ptest.c
+++ v2ptest.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[])
     int i = 0;
     int beep = 0x8000000;
 
-    printf(1, "instruction: virtual: %p, physical %x\n", (void*)&test, v2p((int)&test));
+    printf(1, "instruction: virtual: %p, physicalx\n", (void*)&test, v2p((int)&test));
     printf(1, "local: virtual: %p, physical: %x\n", (void*)&i, v2p((int)&i));
     printf(1, "global: virtual: %p, physical: %x\n", (void*)&j, v2p((int)&j));
     printf(1, "invalid: virtual: %x, physical: %x\n", beep, v2p(beep));
