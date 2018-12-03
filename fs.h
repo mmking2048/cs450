b7104e6627fa9a76256768c173ebb6cb521eb8b4
Michelle King <mmking2048@gmail.com>
2018-12-03 14:26:36 -0600

    completed MP4

--- fs.h
+++ fs.h
@@ -21,9 +21,11 @@ struct superblock {
   uint bmapstart;    // Block number of first free map block
 };
 
-#define NDIRECT 12
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDIRECT 10
+#define SIZEINDIRECT (BSIZE / sizeof(uint))
+#define NINDIRECT (2 * BSIZE / sizeof(uint))
+#define N2INDIRECT (SIZEINDIRECT * SIZEINDIRECT)
+#define MAXFILE (NDIRECT + NINDIRECT + N2INDIRECT)
 
 // On-disk inode structure
 struct dinode {
@@ -32,7 +34,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEV only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+3];   // Data block addresses
 };
 
 // Inodes per block.
